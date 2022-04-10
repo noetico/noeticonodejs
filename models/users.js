bbeeb636aa0ad877d8620f1bacd40e52d7e7f0e8
Kent <zkentz@live.com>
2022-04-10 23:37:20 +0100

    fix_packages.json

--- models/users.js
+++ models/users.js
@@ -1,29 +1,29 @@
-var express = require('express');
-var usercontroller = express.Router();
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-mongoose.connect('mongodb://localhost:27017/appdb');
-var db = mongoose.connection;
+// var express = require('express');
+// var usercontroller = express.Router();
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+// mongoose.connect('mongodb://localhost:27017/appdb');
+// var db = mongoose.connection;
 
-var user = new Schema({
-    username: {
-    type: String,
-    index: true
-    },
-    password: {
-    type: String
-    },
-    email: {
-    type: String
-    },
-    name: {
-    type: String
-    },
-    profileimage: {
-    type: String
-    }
-    });
-module.exports = mongoose.model('User', user);;
+// var user = new Schema({
+//     username: {
+//     type: String,
+//     index: true
+//     },
+//     password: {
+//     type: String
+//     },
+//     email: {
+//     type: String
+//     },
+//     name: {
+//     type: String
+//     },
+//     profileimage: {
+//     type: String
+//     }
+//     });
+// module.exports = mongoose.model('User', user);;
 //     var User = module.exports = mongoose.model('User', UserSchema);
 // module.exports.createUser = function(newUser, callback){
 // newUser.save(callback);
